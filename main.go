eae6efa2c215ca23eda4373cd92a5b479b3470b1
Phillip Zastrow <zastrow.phillip@gmail.com>
2021-04-02 08:50:50 +0200

    Create the create new deck and deal cards function

--- main.go
+++ main.go
@@ -1,16 +1,8 @@
 package main
 
-import "fmt"
-
 func main() {
-	cards := []string{newCard(), "Ace of Diamonds"}
-	cards = append(cards, "Six of Spades")
-
-	for i, card := range cards {
-		fmt.Println(card)
-	}
-}
-
-func newCard() string {
-	return "Five of Diamonds"
+	cards := newDeck()
+	hand, remainingDeck := deal(cards, 5)
+	hand.print()
+	remainingDeck.print()
 }
